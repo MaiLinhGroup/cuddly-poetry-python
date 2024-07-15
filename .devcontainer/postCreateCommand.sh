f0072c6218776bb14d2efe44daf5d2eab307fe41
MaiLinhGroup <9516580+MaiLinhGroup@users.noreply.github.com>
2024-07-15 17:26:04 +0200

    build(codespaces): Remove script bc of missing permission and run directly with devcontainer.json

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-# Create a virtual environment for the project and activate it
-python3 -m venv /workspaces/cuddly-poetry-python/.venv
-source /workspaces/cuddly-poetry-python/.venv/bin/activate
-
-# Install Dependencies into the virtual environment
-poetry install --with dev
