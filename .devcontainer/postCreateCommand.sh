253448b6e473506d504a5b7c8beb629aeea94fb1
MaiLinhGroup <9516580+MaiLinhGroup@users.noreply.github.com>
2024-07-15 15:08:06 +0200

    build(codespaces): Add create and activate virtual env after container creation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 
-# Install Dependencies
+# Create a virtual environment for the project and activate it
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install Dependencies into the virtual environment
 poetry install --with dev
